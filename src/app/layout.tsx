f18561e39641a040ca78313643c4731be93c7bf8
lucas <lucas.stettner@gmail.com>
2023-10-27 14:03:39 -0400

    home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Manrope } from "next/font/google";
 import "./globals.css";
+import clsx from "clsx";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const manrope = Manrope({ subsets: ["latin"], variable: "--font-manrope" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={clsx(inter.variable, manrope.variable)}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import { ConnectKitButton } from "connectkit";
 import { WagmiConfig, createConfig } from "wagmi";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { base } from "viem/chains";
 
 const config = createConfig(
   getDefaultConfig({
     alchemyId: process.env.ALCHEMY_ID, // or infuraId
     walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID!,
     appName: "Grifted",
+    chains: [base],
 
     // Optional
     // appDescription: "Your App Description",
@@ -21,9 +23,18 @@ export default function Home() {
   return (
     <WagmiConfig config={config}>
       <ConnectKitProvider>
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <h1 className="text-xl">hello world</h1>
+        <nav className="full flex items-center justify-end px-[120px] h-[82px]">
           <ConnectKitButton />
+        </nav>
+        <main className="flex flex-col items-center text-center py-12">
+          <h1 className="font-display text-[56px] font-bold -tracking-[1.6px] leading-[110%]">
+            What do you want
+            <br /> to Grift?
+          </h1>
+          <p className="font-body mt-6 text-gray-600">
+            Some kind of description of the service and why you should
+            <br /> connect your wallet and not worry.
+          </p>
         </main>
       </ConnectKitProvider>
     </WagmiConfig>
